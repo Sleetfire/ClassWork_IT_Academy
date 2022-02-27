68d28092129280277a452702d8ae6122664ba1ff
SleetFire <41988252+Sleetfire@users.noreply.github.com>
2022-02-27 21:05:00 +0300

    added database connection

--- src/main/java/by/it/academy/MK_JD2_88_2/hw1/service/UserService.java
+++ src/main/java/by/it/academy/MK_JD2_88_2/hw1/service/UserService.java
@@ -2,41 +2,43 @@ package by.it.academy.MK_JD2_88_2.hw1.service;
 
 import by.it.academy.MK_JD2_88_2.hw1.dto.User;
 import by.it.academy.MK_JD2_88_2.hw1.service.api.IUserService;
+import by.it.academy.MK_JD2_88_2.hw1.storage.DBUserStorage;
+import by.it.academy.MK_JD2_88_2.hw1.storage.UserStorage;
+import by.it.academy.MK_JD2_88_2.hw1.storage.api.IUserStorage;
 
 import java.util.*;
 
 public class UserService implements IUserService {
 
-    private final Map<String, User> userMap = new HashMap<>();
     private static final IUserService instance = new UserService();
+    private final IUserStorage storage = DBUserStorage.getInstance();
 
     private UserService() {
     }
 
     @Override
     public void createUser(User user) {
-        String login = user.getLogin();
-        this.userMap.put(login, user);
+        this.storage.add(user);
     }
 
     @Override
     public List<User> getUsers() {
-        return List.copyOf(this.userMap.values());
+        return this.storage.getAll();
     }
 
     @Override
     public User getUserByLogin(String login) {
-        return this.userMap.get(login);
+        return this.storage.get(login);
     }
 
     @Override
     public int getUserCount() {
-        return getUsers().size();
+        return this.storage.getCount();
     }
 
     @Override
     public void deleteUserByLogin(String login) {
-        this.userMap.remove(login);
+        this.storage.delete(login);
     }
 
     public static IUserService getInstance() {
